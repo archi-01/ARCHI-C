064fdf1f31253ec2fc65b3ec741388d2da306eca
archi-01 <omtrivedi460@gmail.com>
2023-06-09 20:10:13 +0530

    ch.4

---
+++ ch5/..C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int a;
+   clrscr();
+   printf("enter the number :");
+   scanf("%d",&a);
+   if(a>0)
+   {
+	printf("positive number ");
+   }
+   else
+   {
+	printf("negative number ");
+   }
+    getch();
+}
