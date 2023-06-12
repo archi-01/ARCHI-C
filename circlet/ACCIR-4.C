6860aab568bcd70c4e7daf8d128971c87ce2dff3
archi-01 <omtrivedi460@gmail.com>
2023-06-12 19:25:11 +0530

    circlet

---
+++ circlet/ACCIR-4.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int i,j,s,space=0;
+	clrscr();
+	for(i=5;i>=1;i--)
+	{
+	  for(s=1;s<=space;s++)
+		{
+			printf("  ");
+		}
+
+	space++;
+	  for(j=1;j<=i;j++)
+		{
+			printf("%d ",j%2);
+
+		}
+		printf("\n");
+	}
+
+		getch();
+}
+
+
